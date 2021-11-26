150b1c750ee91fa257d655a2d1d9ab5973c1c45e
Damien Erambert <damien@erambert.me>
2021-11-25 22:57:01 -0800

    Update noTopLevelVariables.ts

--- tests/noTopLevelVariables.ts
+++ tests/noTopLevelVariables.ts
@@ -1,6 +1,84 @@
 import * as parser from '@typescript-eslint/parser';
 import {RuleTester} from 'eslint';
-import {noTopLevelVariables} from '../../../lib/rules/noTopLevelVariables';
+import {noTopLevelVariables} from '../lib/rules/noTopLevelVariables';
+
+const valid: RuleTester.ValidTestCase[] = [
+  {
+    code: `
+      export default function () {
+        var foo = 'bar';
+      }
+    `
+  },
+  {
+    code: `
+      export default function () {
+        let foo = 'bar';
+      }
+    `
+  },
+  {
+    code: `
+      export default function () {
+        const foo = 'bar';
+      }
+    `
+  },
+  {
+    code: `
+      const foo = 'bar';
+      export default function () {
+      }
+    `,
+    options: [
+      {
+        kind: ['var']
+      }
+    ]
+  }
+];
+
+const invalid: RuleTester.InvalidTestCase[] = [
+  {
+    code: `
+      var foo = 'bar';
+      export default function () {}
+    `,
+    errors: [
+      {
+        messageId: 'message'
+      }
+    ]
+  },
+  {
+    code: `
+      let foo = 'bar';
+      export default function () {}
+    `,
+    errors: [
+      {
+        messageId: 'message'
+      }
+    ]
+  },
+  {
+    code: `
+      var foobar = 'bar';
+      const foo = 'bar';
+      export default function () {}
+    `,
+    options: [
+      {
+        kind: ['var']
+      }
+    ],
+    errors: [
+      {
+        messageId: 'message'
+      }
+    ]
+  }
+];
 
 new RuleTester({
   parser,
@@ -12,80 +90,16 @@ new RuleTester({
     }
   }
 }).run('no-top-level-variables', noTopLevelVariables, {
-  valid: [
-    {
-      code: `
-        export default function () {
-          var foo = 'bar';
-        }
-      `
-    },
-    {
-      code: `
-        export default function () {
-          let foo = 'bar';
-        }
-      `
-    },
-    {
-      code: `
-        export default function () {
-          const foo = 'bar';
-        }
-      `
-    },
-    {
-      code: `
-        const foo = 'bar';
-        export default function () {
-        }
-      `,
-      options: [
-        {
-          kind: ['var']
-        }
-      ]
-    }
-  ],
-  invalid: [
-    {
-      code: `
-        var foo = 'bar';
-        export default function () {}
-      `,
-      errors: [
-        {
-          messageId: 'message'
-        }
-      ]
-    },
-    {
-      code: `
-        let foo = 'bar';
-        export default function () {}
-      `,
-      errors: [
-        {
-          messageId: 'message'
-        }
-      ]
-    },
-    {
-      code: `
-        var foobar = 'bar';
-        const foo = 'bar';
-        export default function () {}
-      `,
-      options: [
-        {
-          kind: ['var']
-        }
-      ],
-      errors: [
-        {
-          messageId: 'message'
-        }
-      ]
-    }
-  ]
+  valid: valid.map((c) => {
+    return {
+      ...c,
+      code: c.code.trim()
+    };
+  }),
+  invalid: invalid.map((c) => {
+    return {
+      ...c,
+      code: c.code.trim()
+    };
+  })
 });
